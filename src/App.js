821bcbc301359fd85cf0474f10b16df0124e21d8
Yura Ivaniv <ivanivyura@gmail.com>
2022-08-30 17:40:42 +0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+
+import "@aws-amplify/ui-react/styles.css";
+
 import awsConfig from "./aws-exports";
 
 Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div>
-      <h1>welcone to amplify</h1>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
