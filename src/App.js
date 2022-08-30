0dfe7114de7ace4bb946205a0e9aa7a4e24f5d7c
Yura Ivaniv <ivanivyura@gmail.com>
2022-08-30 17:11:07 +0300

    added aws config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import { Amplify } from "aws-amplify";
+import awsConfig from "./aws-exports";
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-   <div>
+    <div>
       <h1>welcone to amplify</h1>
-   </div>
+    </div>
   );
 }
 
